03e1b2ed1be4a248b72b38fd64d8d9f2c9103b85
rise42 <makedonskiy2500@gmail.com>
2018-02-10 11:47:47 +0200

    Dead-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
 import React from 'react'
 
+import Sources from './Sources.js'
+import Display from './Display.js'
+
 export default class App extends React.Component {
-  render() {
+  constructor (props) {
+    super(props)
+    this.state = {
+      availableSources: ['Kappa', 'Keepo'],
+      activeSources: []
+    }
+
+    this.handleSourceChange = this.handleSourceChange.bind(this)
+  }
+
+  render () {
     return (
-      <p>
-        Kappa
-      </p>
+      <div>
+        <Sources availableSources={this.state.availableSources} handleChange={this.handleSourceChange} />
+        <Display activeSources={this.state.activeSources} />
+      </div>
     )
+  }
+
+  handleSourceChange (e) {
+    if (e.target.checked) {
+      const newActive = {activeSources: [e.target.id, ...this.state.activeSources]}
+      this.setState(
+        Object.assign({}, this.state, newActive)
+      )
+      console.log(this.state.activeSources)
+    } else {
+      this.setState(
+        Object.assign({}, this.state, {activeSources: this.stateactiveSources.filter(item => item !== e.target.id) || []})
+      )
+    }
   }
 }
