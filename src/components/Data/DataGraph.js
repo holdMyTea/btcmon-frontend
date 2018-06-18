42b5c7e622f42244af135d1285536b445f179ae9
rise42 <makedonskiy2500@gmail.com>
2018-06-18 23:47:44 +0300

    CSS pieces, a bit of adaptivity

--- src/components/Data/DataGraph.js
+++ src/components/Data/DataGraph.js
@@ -3,60 +3,91 @@ import { LineChart, YAxis, Line } from 'recharts'
 import PropTypes from 'prop-types'
 import moment from 'moment'
 
-const DataGraph = ({isGettingSources, isGettingData, sourcesFailure, dataFailure, selectedSources, data}) => {
-  return isGettingSources ? (<h3>Waiting for the sources update</h3>)
-    : isGettingData || data.length === 0 ? (<h3>Waiting for the data</h3>)
-      : sourcesFailure ? (<h3>Failed to get sources</h3>)
-        : dataFailure ? (<h3>Failed to get data</h3>)
-          : selectedSources.length === 0 ? (<h3>No sources selected</h3>)
-            : (
-              <LineChart width={600} height={300} data={formatData(data)}>
-                <YAxis type="number" domain={computeDomain(data)} width={100} />
-                {
-                  selectedSources.map(
-                    source =>
-                      (<Line dataKey={source} key={source} type="monotone"/>)
-                  )
-                }
-              </LineChart>
-            )
-}
-
-const formatData = data => {
-  const formattedData = []
-
-  for (let i = 0; i < data.reduce((acc, val) => Math.min(acc, val.data.length), Infinity); i++) {
-    const element = {
-      timestamp: moment(
-        data.reduce((acc, val) =>
-          (acc += moment(val.data[i].timestamp).unix()), 0
-        ) / data.length).format()
+class DataGraph extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      width: 0,
+      height: 0
     }
-
-    data.forEach(source => (
-      element[source.name] = source.data[i].value
-    ))
-
-    formattedData.push(element)
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
   }
 
-  return formattedData
-}
+  componentDidMount () {
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
+  }
 
-const computeDomain = data => {
-  let min = Infinity
-  let max = -Infinity
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.updateWindowDimensions)
+  }
 
-  data.forEach(source => {
-    source.data.forEach(e => {
-      min = Math.min(min, e.value)
-      max = Math.max(max, e.value)
+  render () {
+    return this.props.isGettingSources ? (<h3>Waiting for the sources update</h3>)
+      : this.props.isGettingData || this.props.data.length === 0 ? (<h3>Waiting for the data</h3>)
+        : this.props.sourcesFailure ? (<h3>Failed to get sources</h3>)
+          : this.props.dataFailure ? (<h3>Failed to get data</h3>)
+            : this.props.selectedSources.length === 0 ? (<h3>No sources selected</h3>)
+              : (
+                <LineChart width={this.state.width * 0.75}
+                  height={this.state.height * 0.5}
+                  data={this.formatData(this.props.data)}>
+
+                  <YAxis type="number" domain={this.computeDomain(this.props.data)} width={100} />
+                  {
+                    this.props.selectedSources.map(
+                      source =>
+                        (<Line dataKey={source} key={source} type="monotone"/>)
+                    )
+                  }
+
+                </LineChart>
+              )
+  }
+
+  formatData (data) {
+    const formattedData = []
+
+    for (let i = 0; i < data.reduce((acc, val) => Math.min(acc, val.data.length), Infinity); i++) {
+      const element = {
+        timestamp: moment(
+          data.reduce((acc, val) =>
+            (acc += moment(val.data[i].timestamp).unix()), 0
+          ) / data.length).format()
+      }
+
+      data.forEach(source => (
+        element[source.name] = source.data[i].value
+      ))
+
+      formattedData.push(element)
+    }
+
+    return formattedData
+  }
+
+  computeDomain (data) {
+    let min = Infinity
+    let max = -Infinity
+
+    data.forEach(source => {
+      source.data.forEach(e => {
+        min = Math.min(min, e.value)
+        max = Math.max(max, e.value)
+      })
     })
-  })
 
-  const diff = (max - min) * 0.05
+    const diff = (max - min) * 0.05
 
-  return [min - diff, max + diff]
+    return [min - diff, max + diff]
+  }
+
+  updateWindowDimensions () {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
 }
 
 DataGraph.propTypes = {
